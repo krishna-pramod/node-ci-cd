bcc237f94130bb1cf572762a64d8698d6c2c253b
Krishna Pramod <krishna.pramod2@gmail.com>
2020-09-29 23:19:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ci-cd deployment
+
+This is an example of deploying nodejs application to google app engine
---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('express')();
+
+const port = process.env.PORT || 3000;
+
+app.get('*', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
