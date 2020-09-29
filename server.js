2edc7ab543ea90cf7c364f695da261dd8b4d8741
Krishna Pramod <krishna.pramod2@gmail.com>
2020-09-29 23:47:45 +0530

    Fixed a typ

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV === 'produciton') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(`${__dirname}/client/dist`));
 
   app.get('*', (req, res) => {
