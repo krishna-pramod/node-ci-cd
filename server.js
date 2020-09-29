80b71b1cde42b511561373d91c8b88783ab9df36
Krishna Pramod <krishna.pramod2@gmail.com>
2020-09-29 23:29:00 +0530

    Changes to server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('express')();
 const port = process.env.PORT || 3000;
 
 app.get('*', (req, res) => {
-  res.send('Hello World from google cloud');
+  res.send('Hello World from google cloud platform');
 });
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
